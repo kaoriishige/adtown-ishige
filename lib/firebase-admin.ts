febb55b997948070bca0e61cb96149544daa729e
kaoriishige <adtown001@gmail.com>
2025-09-12 12:30:01 +0900

    Final revision of Minna no Nasu app

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -10,14 +10,14 @@ if (!admin.apps.length) {
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    // Storageを正しく利用するために、バケット名を指定します
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
 }
 
-// 他のファイルが必要としている関数をすべて定義してエクスポートします
+// 他のファイルが必要としている関数とオブジェクトをすべて定義してエクスポートします
 const getAdminAuth = (): admin.auth.Auth => admin.auth();
 const getAdminDb = (): admin.firestore.Firestore => admin.firestore();
 const getAdminStorageBucket = () => admin.storage().bucket();
 
-export { getAdminAuth, getAdminDb, getAdminStorageBucket };
+// ▼▼▼ 今回の修正点：`admin` 本体もエクスポートに加える ▼▼▼
+export { admin, getAdminAuth, getAdminDb, getAdminStorageBucket };
