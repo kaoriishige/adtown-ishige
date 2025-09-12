9d9a9eaf9dad1700fad54ca041db3e9efde98222
kaoriishige <adtown001@gmail.com>
2025-09-12 12:17:17 +0900

    Final revision of Minna no Nasu app

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,17 +2,22 @@ import * as admin from 'firebase-admin';
 
 // Appインスタンスを一度だけ初期化するための工夫
 if (!admin.apps.length) {
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  };
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-    }),
+    credential: admin.credential.cert(serviceAccount),
+    // Storageを正しく利用するために、バケット名を指定します
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
 }
 
-// 他ファイルが要求している関数を定義してエクスポート
+// 他のファイルが必要としている関数をすべて定義してエクスポートします
 const getAdminAuth = (): admin.auth.Auth => admin.auth();
 const getAdminDb = (): admin.firestore.Firestore => admin.firestore();
+const getAdminStorageBucket = () => admin.storage().bucket();
 
-export { getAdminAuth, getAdminDb };
+export { getAdminAuth, getAdminDb, getAdminStorageBucket };
