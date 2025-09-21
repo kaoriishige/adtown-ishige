39ab891738b850a25d784b6239b8cc6a539930e1
kaoriishige <adtown001@gmail.com>
2025-09-21 18:49:54 +0900

    fix: add Netlify domain to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,24 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'scdn.line-apps.com',
-        port: '',
-        pathname: '/n/line_add_friends/btn/ja.png',
-      },
-      {
-        protocol: 'https',
-        hostname: 'minna-no-nasu-app.netlify.app',
-        port: '',
-        pathname: '/images/**', // ← Netlifyの公開画像を許可
-      },
-    ],
+    unoptimized: true, // ← これだけでOK
   },
 };
 
 module.exports = nextConfig;
+
+
+
 
 
 
