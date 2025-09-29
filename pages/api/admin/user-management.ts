3b81e671be67f339751be4f70c774dc7ed8f1060
kaoriishige <adtown001@gmail.com>
2025-09-30 05:36:48 +0900

    Final revision of Minna no Nasu app

--- pages/api/admin/user-management.ts
+++ pages/api/admin/user-management.ts
@@ -4,8 +4,8 @@ import nookies from 'nookies';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
-    return res.status(405).end('Method Not Allowed');
-  }
+    return res.status(405).end('Method Not Allowed')
+    }
 
   try {
     const cookies = nookies.get({ req });
