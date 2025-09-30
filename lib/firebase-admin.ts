b203b796b580ceda59af178cee48200b3f52a550
kaoriishige <adtown001@gmail.com>
2025-09-30 11:19:30 +0900

    Final revision of Minna no Nasu app

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,26 +1,29 @@
 import * as admin from 'firebase-admin';
-// 型情報をインポートする (ここが追加点)
-import { Auth } from 'firebase-admin/auth';
-import { Firestore } from 'firebase-admin/firestore';
-import { getAuth } from 'firebase-admin/auth';
-import { getFirestore } from 'firebase-admin/firestore';
+import { Auth, getAuth } from 'firebase-admin/auth';
+import { Firestore, getFirestore } from 'firebase-admin/firestore';
+import { Storage, getStorage } from 'firebase-admin/storage'; // Storage用のimportを追加
 
 export const initializeAdminApp = () => {
   if (admin.apps.length > 0) {
     return;
   }
-
   const serviceAccount = JSON.parse(
     process.env.FIREBASE_SERVICE_ACCOUNT_JSON as string
   );
-
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    // StorageバケットのURLを初期化設定に追加
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
 };
 
 initializeAdminApp();
 
-// 定数に型を明記する (ここが変更点)
 export const adminAuth: Auth = getAuth();
 export const adminDb: Firestore = getFirestore();
+export const adminStorage: Storage = getStorage(); // Storageインスタンスをエクスポート
+
+// 他ファイルが必要としている関数
+export const getAdminStorageBucket = () => {
+  return adminStorage.bucket(); // デフォルトのバケットを返す
+};
