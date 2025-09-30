f6457c93c2c4738f4675b303f8052e14986c9d1a
kaoriishige <adtown001@gmail.com>
2025-09-30 10:53:51 +0900

    Fix all remaining API errors

--- pages/api/test-env.ts
+++ pages/api/test-env.ts
@@ -2,9 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // ターミナルに環境変数の内容を表示させる
-  console.log("--- .env.local テストの開始 ---");
+  console.log("--- .env.local テスト開始 ---");
   console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
-  console.log("--- テストの終了 ---");
+  console.log("--- テスト終了 ---");
   
   // ブラウザに結果を表示させる
   res.status(200).json({ 
