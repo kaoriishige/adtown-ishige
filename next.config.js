30036fe4c090cc3f63412766132d86697c431715
kaoriishige <adtown001@gmail.com>
2025-10-06 13:09:54 +0900

    Final revision of Minna no Nasu app

--- next.config.js
+++ next.config.js
@@ -1,18 +1,35 @@
 // next.config.js
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   webpack: (config, { isServer }) => {
+    // クライアントサイドでのみ設定を適用
     if (!isServer) {
       config.resolve.fallback = {
         fs: false,
         net: false,
         tls: false,
+        child_process: false,
+        http2: false,
+        // ここに他のNode.jsモジュールを追加
       };
     }
+
+    // WebAssemblyの問題に対応するため、実験的な設定を追加
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+
     return config;
   },
 };
