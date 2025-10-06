15ca115c0d3b3ec442fad187919f0c5116c59587
kaoriishige <adtown001@gmail.com>
2025-10-06 12:36:22 +0900

    Final revision of Minna no Nasu app

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // ← これだけでOK
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   },
 };
 
 module.exports = nextConfig;
-
 
 
 
