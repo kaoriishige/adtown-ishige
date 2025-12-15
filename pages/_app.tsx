68d3913f0b45472bc8a03180e49891b51ba8d827
kaoriishige <adtown001@gmail.com>
2025-12-16 03:49:46 +0900

    Final revision of Minna no Nasu app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 // pages/_app.tsx
 
-import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+// 1. AuthProvider をインポート
+import { AuthProvider } from '../contexts/AuthContext'; 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // 2. AuthProvider で Component を囲む
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
